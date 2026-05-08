4fa09ea854159ba84f55654ef26a6bdac81ef6fd
VladDyvo <vlad-dyvohryts@hotmail.com>
2026-05-08 19:51:49 +0200

    Update roi_certificazioni.sql

--- metrica_tre/roi_certificazioni.sql
+++ metrica_tre/roi_certificazioni.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    certifications,
+    ROUND(AVG(salary), 2) as Avg_Salary,
+    ROUND(AVG(salary) - LAG(AVG(salary)) OVER (ORDER BY certifications), 2) as Incremento_Marginale
+FROM job_salary_prediction_dataset
+GROUP BY certifications
+ORDER BY certifications ;
