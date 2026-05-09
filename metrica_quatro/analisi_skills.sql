c407380bf38d0f940487d3dcd3565d82e84215a7
VladDyvo <vlad-dyvohryts@hotmail.com>
2026-05-09 14:47:40 +0200

    Update analisi_skills.sql

--- metrica_quatro/analisi_skills.sql
+++ metrica_quatro/analisi_skills.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    skills_count AS Num_Competenze,
+    ROUND(AVG(salary), 2) as Stipendio_Medio,
+    COUNT(*) as Numero_Professionisti 
+FROM job_salary_prediction_dataset
+GROUP BY skills_count
+ORDER BY skills_count;
